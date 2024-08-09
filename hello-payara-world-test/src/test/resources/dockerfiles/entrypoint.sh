fd3a0a82de0211ac78fd4146284615267423e777
Gael Abadin <gael.abadin@gmail.com>
2024-08-09 18:26:14 +0200

    Update entrypoint.sh - add --debug flag to asadmin start-domain command so JVM debugger to exposed port 9009

--- hello-payara-world-test/src/test/resources/dockerfiles/entrypoint.sh
+++ hello-payara-world-test/src/test/resources/dockerfiles/entrypoint.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 if [ -f /tmp/firstrun ]; then
     echo "deploy /opt/payara6/hello.ear" >> "first_run_post_boot_commands"
-    asadmin start-domain -v --postbootcommandfile=first_run_post_boot_commands
+    asadmin start-domain --debug -v --postbootcommandfile=first_run_post_boot_commands
     rm /tmp/firstrun
 else
     # Start payara
